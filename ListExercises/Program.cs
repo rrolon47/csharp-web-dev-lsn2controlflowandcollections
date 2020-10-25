82b46ace48c4a21dc2ba34050f455a855f254c60
Rachel <rachel.rolon@gmail.com>
2020-10-24 19:49:03 -0500

    bonus done

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography.X509Certificates;
 
@@ -26,7 +27,12 @@ namespace ListExercises
             Console.WriteLine(Sum(newList));
 
             //Write a static method to print out each word in a list that has exactly 5 letters.
+            //Modify your code to prompt the user to enter the word length for the search
             List<string> wordList = new List<string> { "the", "cat", "fight", "blue" };
+            
+            string greenEggs = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+            List<string> stringToList = greenEggs.Split(" ").ToList();
+
             static void LetterWord(List<string> arg)
             {
                 Console.WriteLine("Enter a number:");
@@ -41,9 +47,15 @@ namespace ListExercises
                 }
             }
 
-            //Modify your code to prompt the user to enter the word length for the search
             
-                LetterWord(wordList);
+            
+            LetterWord(wordList);
+            LetterWord(stringToList);
+
+
+            //Bonus
+            //https://stackoverflow.com/questions/5011467/convert-string-to-liststring-in-one-line
+            //List<string> result = names.Split(',').ToList();
 
         }
     }
